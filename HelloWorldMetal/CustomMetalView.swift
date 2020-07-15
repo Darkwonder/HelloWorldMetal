76f50c709895b1b0bfb798e35897093c472fc79b
Mladen Mikic <mb87.apple@gmail.com>
2020-07-15 17:51:52 +0200

    Made redrawn more performand for multiple calls

--- HelloWorldMetal/CustomMetalView.swift
+++ HelloWorldMetal/CustomMetalView.swift
@@ -17,6 +17,15 @@ class CustomMetalView: NSView,
     // https://developer.apple.com/documentation/quartzcore/cametallayer
     private var metalLayer: CAMetalLayer! = nil
     
+    // https://developer.apple.com/documentation/metal/mtlcommandqueue
+    private var commandQueue: MTLCommandQueue! = nil
+    
+    // https://developer.apple.com/documentation/metal/mtlrenderpassdescriptor
+    private let passDescriptor = MTLRenderPassDescriptor()
+    
+    // https://developer.apple.com/documentation/metal/mtlrenderpasscolorattachmentdescriptor
+    private var colorAttachment: MTLRenderPassColorAttachmentDescriptor! = nil
+    
     // MARK: - Init
     
     override public init(frame:NSRect) {
@@ -57,33 +66,49 @@ class CustomMetalView: NSView,
     func assignDevice(_ device: MTLDevice) {
         self.metalLayer.device = device
         print("\nDevice \(device.name) has been set to: \(self).")
+        // One time creation
+        
+        prepareForReDrawing()
+    }
+    
+    private func prepareForReDrawing() {
+        // Do expensive one time setup code here:
+        guard let commandQueue = self.metalLayer.device?.makeCommandQueue() else { return }
+        self.commandQueue = commandQueue
+        
+        
+        self.colorAttachment = self.passDescriptor.colorAttachments[0]
+       
+        colorAttachment?.loadAction = .clear
+        colorAttachment?.storeAction = .store
+        colorAttachment?.clearColor = MTLClearColor(red: 1.0, green: 0.0, blue: 0.0, alpha: 0.0)
     }
     
     func reDraw() {
         
-        // https://developer.apple.com/documentation/metal/mtlcommandqueue
-        guard let commandQueue = self.metalLayer.device?.makeCommandQueue() else { return }
+        guard let commandQueue = self.commandQueue else { return }
+                
         // https://developer.apple.com/documentation/quartzcore/cametaldrawable
         guard let drawable = self.metalLayer.nextDrawable() else { return }
         
         // https://developer.apple.com/documentation/metal/mtltexture
         let texture = drawable.texture
         
-        // https://developer.apple.com/documentation/metal/mtlrenderpassdescriptor
-        let passDescriptor = MTLRenderPassDescriptor()
-        
-        // https://developer.apple.com/documentation/metal/mtlrenderpasscolorattachmentdescriptor
-        let colorAttachment = passDescriptor.colorAttachments[0]
-        colorAttachment?.texture = texture
-        colorAttachment?.loadAction = .clear
-        colorAttachment?.storeAction = .store
-        colorAttachment?.clearColor = MTLClearColor(red: 1.0, green: 0.0, blue: 0.0, alpha: 0.0)
+        guard let colorAttachment = self.colorAttachment else { return }
+        colorAttachment.texture = texture
         
         // https://developer.apple.com/documentation/metal/mtlcommandbuffer
         let commandBuffer = commandQueue.makeCommandBuffer()
         
         // https://developer.apple.com/documentation/metal/mtlrendercommandencoder
         let commandEncoder = commandBuffer?.makeRenderCommandEncoder(descriptor: passDescriptor)
+        
+        // Adding few single commands:
+        // commandEncoder. ...
+        
+        // Adding several commands from different sources:
+        // self.renderPatch.forEach{ $0.renderWith(commandEncoder) }
+        
         commandEncoder?.endEncoding()
         
         commandBuffer?.present(drawable)
